bde09d7d856e4a0743b35fe68cf3ec2d50b88cdd
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2025-02-10 14:42:26 -0500

    Update Restart-VMs.ps1

--- Restart-VMs.ps1
+++ Restart-VMs.ps1
@@ -12,8 +12,12 @@ Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPas
 
 # List of VMs to restart
 $vmList = @(
-    "284-01", "284-02", "284-03", "284-04", "284-05", "284-06", "284-07", "284-08", "284-09", "284-10",
-    "284-11", "284-12", "284-13", "284-14", "284-15", "284-16", "284-17", "386-00", "386-01", "358-01"
+    "25-alexanmf", "25-alir2", "25-almafrtk", "25-barillnt", "25-beimesca", "25-benzink", "25-blairc", "25-bosticdj", "25-bowman37", "25-brownc61",
+    "25-burkhejl", "25-careyjr", "25-clarkap2", "25-cryselka", "25-davenpr2", "25-diallof2", "25-ehlerjl", "25-eschenea", "25-fangmaj2", "25-ferrana", 
+    "25-fishe192", "25-gartnea", "25-guzmanma", "25-hertleac", "25-hibbarkm", "25-hoganec", "25-jonesm64", "25-khanalb", "25-leachh", "25-lieuhk", 
+    "25-mcclela", "25-mooreac5", "murrayt6", "25-nguyenjm", "25-nolen", "25-parsonjt", "25-perezi3", "25-rimalgp", "25-rodrig99", "25-roger142",
+    "25-samals", "25-sanwuya", "25-sapkotp", "25-smith624", "25-stidhalt", "25-thiekend", "25-vontrodl", "25-wattsts", "25-widenemg", "25-wilso682", 
+    "25-yharbrm", "25-hibbardkm"
 )
 
 # Loop through each VM name and restart the VM if found
